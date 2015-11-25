323d11b6d274d0e3741b4cbe853fbd90edde4067
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-11-25 00:48:10 -0700

    Created basic package info.
    Signed-off-by: TheDudeFromCI <linktheblademaster@yahoo.com>

---
+++ Worlds/src/net/worlds/basic/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This world is a very basic environment designed as a simple eat/kill environment. These are quite common for basic artifical intelligence agents.
+ *
+ * @author thedudefromci
+ */
+package net.worlds.basic;
